d48307e7afaaeb67f446497c04f448470548b77d
Carlos Zaragoza beato <71204931+CarlosZaragozaBeato@users.noreply.github.com>
2024-01-29 07:12:34 +0100

    s

--- learn_go/go_with_tests/02_integers/adder_test.go
+++ learn_go/go_with_tests/02_integers/adder_test.go
@@ -1,38 +1,21 @@
-package main 
+package integers 
 
 import "testing"
 
-func AsserEqual[T comparable](t *testing.T, got, want T) {
-  t.Helper()
+func TestAdder(t *testing.T) {
 
-  if got != want {
-    t.Errorf("got %v, want %v", got, want)
+
+  sum := Add(2, 2)
+  expected := 4 
+
+  if sum != expected {
+    t.Errorf("Expected '%d' but got '%d'", expected, sum)
   }
+
 }
 
 
-func AssertNotEqual[T comparable](t *testing.T, got want){
-  t.Helper()
 
-  if got == want {
-    t.Errorf("Didn't want %v", got)
-  }
+func Add(x, y int) int {
+  return x + y
 }
-
-func AssertTrue(t * testing.T, got bool){
-  t.Helper()
-
-  if !got {
-    t.Errorf("got %v, want true",got)
-  }
-}
-
-func AssertFalse(t *testing.T, got bool) {
-  t.Helper()
-
-  if got {
-    t.Errorf("got %v, want false", got)
-  }
-}
-
-
---
+++ learn_go/go_with_tests/03_iteration/repeat.go
@@ -0,0 +1,7 @@
+
+package iteration
+
+
+func  Repeat (characted string) string {
+  return ""
+}
